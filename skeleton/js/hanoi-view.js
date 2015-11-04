5115c1b8b80034f99f4ae1c51307b7764bfddeca
App Academy Student <appacademy@Apps-Mac-mini-33.local>
2015-11-04 17:15:25 -0500

    Implement display of rings.

--- skeleton/js/hanoi-view.js
+++ skeleton/js/hanoi-view.js
@@ -5,13 +5,14 @@ var View = Hanoi.View = function(game, $el) {
   this.game = game;
   this.$el = $el;
   this.setupTowers();
+  this.render();
 }
 
 Hanoi.View.prototype.setupTowers = function() {
 
 
   for (var tower = 1; tower < 4; tower++) {
-    var $ul = $("<ul>").addClass("tower").data("num", tower);
+    var $ul = $("<ul>").addClass("tower group").data("num", tower);
     for (var disc=1; disc < 4; disc++) {
       $("<li>").addClass("disc" + disc).appendTo($ul);
     }
@@ -21,10 +22,18 @@ Hanoi.View.prototype.setupTowers = function() {
 }
 
 Hanoi.View.prototype.render = function () {
+  // var towers = this.game.towers;
+  var towers = [[3],[2,1],[]];
 
-
-
-
+  for (var i = 0; i < 3; i++) {
+    var $tower = $($("ul")[i]);
+        $($tower.find("li")).removeClass();
+    for (var j = 0; j < 3; j++) {
+      if (j < towers[i].length) {
+        $($($tower.find("li"))[2-j]).addClass("disc" + towers[i][j]);
+      }
+    }
+  }
 
 }
 
